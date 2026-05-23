cf9fda7b161041ec66af0d5683698d54231be024
hengvvang <hengvvang@gmail.com>
2026-05-24 06:27:08 +0800

    chore: remove unused files, apply new gitignore rules & update layout logic

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 import { serve } from "bun";
 
 const server = serve({
-  port: 9191, // 修改为 6506
+  port: 9191,
   async fetch(req) {
     const url = new URL(req.url);
     const path = url.pathname;
