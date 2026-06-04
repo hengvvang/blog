7874ce0689a302046b088be361a4ce4c3960e449
hengvvang <hengvvang@gmail.com>
2026-06-04 22:22:09 +0800

    fix: add executable permission to mdbook binary in build-cloudflare.sh

--- build-cloudflare.sh
+++ build-cloudflare.sh
@@ -10,6 +10,7 @@ echo "=== Installing mdbook ==="
 # Download the mdbook binary (linux x86_64)
 MDBOOK_VERSION="v0.4.40"
 curl -sL "https://github.com/rust-lang/mdBook/releases/download/${MDBOOK_VERSION}/mdbook-${MDBOOK_VERSION}-x86_64-unknown-linux-gnu.tar.gz" | tar -xz
+chmod +x mdbook
 # Add current directory to PATH so mdbook can be executed
 export PATH="$PWD:$PATH"
 
